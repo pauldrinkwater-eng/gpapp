629fb2c69c8e1a7c5b38079909b583b4d19578e6
pauldrinkwater-eng <paul.drinkwater@nhs.net>
2025-09-11 16:59:16 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,6 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 bg-nhs-blue text-white">
       <div className="mx-auto max-w-lg px-4 h-12 flex items-center justify-between">
-        {/* NHS mark */}
         <div className="flex items-center gap-2">
           <div className="bg-white rounded-[3px] px-1.5 py-0.5">
             <span className="block text-[14px] font-extrabold leading-none text-nhs-blue tracking-[0.02em]">
@@ -14,8 +13,6 @@ export default function Header() {
           </div>
           <span className="text-sm/none opacity-90">Malthouse Surgery</span>
         </div>
-
-        {/* Right action (optional) */}
         <a
           href="https://www.malthousesurgery.co.uk/"
           target="_blank"
