9e650833f8dc994505be95e03eca57f622de4040
pauldrinkwater-eng <paul.drinkwater@nhs.net>
2025-09-14 15:35:06 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="bg-white border-b border-gray-200">
-      <div className="mx-auto flex max-w-5xl items-center">
+      <div className="mx-auto flex max-w-5xl items-center justify-center py-2">
         {/* Cover photo (clickable, links home) */}
-        <Link href="/" className="flex-grow flex justify-center">
+        <Link href="/" className="flex justify-center">
           <img
             src="/icons/Google-Cover-Photo.jpeg"
             alt="Malthouse Surgery Cover"
-            className="h-36 object-contain"
+            className="h-20 object-contain"
           />
         </Link>
       </div>
